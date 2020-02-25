d2fbf4238faf4fe9b4fce5b2c35f78e56931743b
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-25 16:48:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,3 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  ans = {}
-  data.each do |key, value|
-    value.each do |property, names|
-      names.each do |name|
-        
-        if !ans[name]
-          ans[name] = {}
-        end
-        
-        if !ans[name][key]
-          ans[name][key] = []
-        end
-        
-        ans[name][key].push(property.to_s)
-      end
-    end
-  end
-  ans
 end
